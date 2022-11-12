feb8cba0827d55955652e6c9d112d61e70f50537
wangbin <wangbin@163.com>
2022-11-12 23:45:13 +0800

    添加word文件

---
+++ src/main/java/com/bjpowernode/Test02.java
@@ -0,0 +1,11 @@
+package com.bjpowernode;
+
+/**
+ * wangbin
+ * 2022.11.12
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("我是一个测试文件");
+    }
+}
